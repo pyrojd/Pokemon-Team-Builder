0370b8d695dfc9275471a963fd9f8ef698b07c4b
pyrojd <browndog2448@gmail.com>
2017-03-08 11:37:31 -0500

    Add files via upload

--- Pokemon Team Builder/Website/Pages/Login.aspx
+++ Pokemon Team Builder/Website/Pages/Login.aspx
@@ -1,52 +1,27 @@
-﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Login.aspx.vb" Inherits="Pages_Login" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Pages_Login" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <title>Log-in Page</title>
     <link type="text/css" rel="stylesheet" href="../Styles/StyleSheet.css" />
-    <link type="text/css" rel="stylesheet" href="../Styles/AccountStyle.css" />
 </head>
 <body>
     <ul id="MainNav">
         <li><a href="index.html">Home</a></li>
         <li><a href="">Design Pokémon</a></li>
         <li><a href="">Design Party</a></li>
-        <li class="Acc"><a href="Account.aspx">Account</a></li>
+        <li class="Acc"><a href="Login.aspx">Account</a></li>   
     </ul>
+
     <form id="form1" runat="server">
-    <div class="accContent" aria-orientation="vertical" spellcheck="True">
-
-
-
-        <br />
-
-
-
-        <asp:Label ID="Label1" runat="server" Text="Username(Max 25) : " Height="22px"></asp:Label>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:TextBox ID="Username" runat="server" MaxLength="25"></asp:TextBox>
-        <br />
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <br />
-        <asp:Label ID="Label3" runat="server" Height="22px" Text="Password(Max 20) :"></asp:Label>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:TextBox ID="Password" runat="server" MaxLength="20" Type="password"></asp:TextBox>
-        <br />
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <br />
-        <br />
-        <asp:Button ID="Login" runat="server" Text="Login" OnClick="Login_Click" Width="100px" />
-
-
-
-    &nbsp;&nbsp;&nbsp;
-        <asp:Button ID="Register" runat="server" Text="Register" Width="100px" OnClick="Register_Click" />
-        <br />
-
-
-
+    <div>
+    
     </div>
+        <asp:Login ID="Login1" runat="server" >
+        </asp:Login>
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" ValidationGroup="Login1" />
     </form>
 </body>
 </html>
---
+++ Pokemon Team Builder/Website/Pages/Login.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RegisterUser(object sender, EventArgs e)
+    {
+        int userId = 0;
+        using (SqlConnection con = new SqlConnection("Data Source=stusql;Initial Catalog=EnterpriseJDW350;Integrated Security=true"))
+        {
+            using (SqlCommand cmd = new SqlCommand("Insert_User"))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                    //cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    //cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                    cmd.Connection = con;
+                    con.Open();
+                    userId = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                }
+            }
+            string message = string.Empty;
+            switch (userId)
+            {
+                case -1:
+                    message = "Username already exists.\\nPlease choose a different username.";
+                    break;
+                case -2:
+                    message = "Supplied email address has already been used.";
+                    break;
+                default:
+                    message = "Registration successful.";
+                    break;
+            }
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + message + "');", true);
+        }
+    }
+
+    public int Validate_Login(String Username, String Password)
+    {
+        SqlConnection con = new SqlConnection(@"User id=sa;Password=Dotnetfunda;Server=VUYISWA\VUYISWA;Database=Forum");
+
+        SqlCommand cmdselect = new SqlCommand();
+
+        cmdselect.CommandType = CommandType.StoredProcedure;
+
+        cmdselect.CommandText = "[dbo].[prcLoginv]";
+
+        cmdselect.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = Username;
+
+        cmdselect.Parameters.Add("@UPassword", SqlDbType.VarChar, 50).Value = Password;
+
+        cmdselect.Parameters.Add("@OutRes", SqlDbType.Int, 4);
+
+        cmdselect.Parameters["@OutRes"].Direction = ParameterDirection.Output;
+
+        cmdselect.Connection = con;
+        int Results = 0;
+
+        try
+
+        {
+            con.Open();
+
+            cmdselect.ExecuteNonQuery();
+            Results = (int)cmdselect.Parameters["@OutRes"].Value;
+
+        }
+        catch (SqlException ex)
+
+        {
+
+            //lblMessage.Text = ex.Message;
+        }
+        finally
+
+        {
+            cmdselect.Dispose();
+            if (con != null)
+
+            {
+
+                con.Close();
+
+            }
+        }
+        return Results;
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+
+    {
+
+
+
+        int Results = 0;
+        /*
+        if (txtUsername.Text != string.empty && txtPassword.Text != string.empty)
+
+        {
+
+            Results = Validate_Login(txtUsername.Text.trim(), txtPassword.Text.trim());
+
+            if (Results == 1)
+
+            {
+
+                lblMessage.Text = "Login is Good, Send the User to another page or enable controls";
+
+            }
+
+            else
+
+            {
+
+                lblMessage.Text = "Invalid Login";
+
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+
+                //Dont Give too much information this might tell a hacker what is wrong in the login
+
+            }
+
+        }
+
+        else
+
+        {
+
+            lblMessage.Text = "Please make sure that the username and the password is Correct";
+
+        }
+        */
+
+    }
+}
